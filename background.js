cac7beeed0782b5d189282dc0ee225d5b625ff07
Yuki Nagato <37764578+YUKI2eN3e@users.noreply.github.com>
2022-03-13 08:33:35 +0900

    No redirect when visiting code.jquery.com website

--- background.js
+++ background.js
@@ -1,7 +1,7 @@
 var host = "cdnjs.cloudflare.com/ajax/libs/jquery/";
 chrome.webRequest.onBeforeRequest.addListener(
     function(details) {
-		if(details.url.includes("://code.jquery.com")){
+		if(details.url.includes("://code.jquery.com/jquery-")){
 			var vnum = details.url.match(/([0-9]\.[0-9]\.[0-9])/)[0]
 			var protocall = details.url.match(/^.*\/\//)
 			return {redirectUrl: protocall + host + vnum + (details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]).replace('-'+vnum,'')};
