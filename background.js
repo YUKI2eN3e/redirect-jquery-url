e7128bbab79b8cb8429501b82ff378c3c9f8861f
Yuki Nagato <37764578+YUKI2eN3e@users.noreply.github.com>
2022-03-13 06:26:30 +0900

    first commit

---
+++ background.js
@@ -0,0 +1,20 @@
+var host = "cdnjs.cloudflare.com/ajax/libs/jquery/";
+chrome.webRequest.onBeforeRequest.addListener(
+    function(details) {
+		if(details.url.includes("://code.jquery.com")){
+			var vnum = details.url.match(/([0-9]\.[0-9]\.[0-9])/)[0]
+			var protocall = details.url.match(/^.*\/\//)
+			return {redirectUrl: protocall + host + vnum + (details.url.match(/^https?:\/\/[^\/]+([\S\s]*)/)[1]).replace('-'+vnum,'')};
+		}
+		else{
+			return
+		}
+    },
+    {
+        urls: [
+            "*://*/*"
+        ],
+        types: ["script"]
+    },
+    ["blocking"]
+);
